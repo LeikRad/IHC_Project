58c3bb05374d15e8f6014c711027143df7fdb8a0
AlexandreCotorobai <alexandrecotorobai@gmail.com>
2023-05-09 20:01:05 +0100

    Announcements done

--- app/src/routes.js
+++ app/src/routes.js
@@ -6,6 +6,7 @@ import Layout, { CleanLayout } from "./layouts/Layout";
 import TestingPage from "./pages/TestingPage";
 import TestingPage2 from "./pages/TestingPage2";
 import UserM from "./pages/UserMainPage";
+import Announce from "./pages/Announcement";
 
 
 const routes = ({ noNav }) => [
@@ -16,6 +17,7 @@ const routes = ({ noNav }) => [
             { path: "/", element: <TestingPage /> },
             { path: "/profile", element: <TestingPage2 /> },
             { path: "/main", element: <UserM />},
+            { path: "/announce", element: <Announce />},    
             { path: "*", element: <Navigate to="/" /> },
         ],
     }
