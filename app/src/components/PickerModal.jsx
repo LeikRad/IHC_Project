39f8e2a4491488b54ab886f42e23580486df64a2
Júlia Abrantes <45010063+JuliaAbrantes@users.noreply.github.com>
2023-05-14 01:14:50 +0100

    left pannel and date picker

---
+++ app/src/components/PickerModal.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import TimePicker from 'react-time-picker'
+import 'react-modern-calendar-datepicker/lib/DatePicker.css';
+import { Calendar } from "react-modern-calendar-datepicker";
+import 'react-time-picker/dist/TimePicker.css';
+import 'react-clock/dist/Clock.css';
+
+
+const PickerModal = () => {
+  const [selectedDays, setSelectedDays] = useState(null);
+  const [picker, SetPicker] = useState(false);
+  const [value, onChange] = useState('10:00');
+
+  return (
+    <>
+      <input type="checkbox" id="my-modal" className="modal-toggle" />
+      <div className="modal">
+        <div className="modal-box">
+          <button className="btn btn-sm btn-circle absolute left-2 top-2">«</button>
+          <label htmlFor="my-modal" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+          
+          <div className="flex flex-row gap-4 pt-10">
+            <div className="basis-[50%]">
+              <h3 className="font-bold text-lg">escolha o dia</h3>
+              <Calendar
+                value={selectedDays}
+                onChange={setSelectedDays}
+                shouldHighlightWeekends
+              />
+            </div>
+
+            <div className="basis-[30%]">
+              <h3 className="font-bold text-lg">escolha a hora</h3>
+              <TimePicker onChange={onChange} value={value} />
+            </div>
+
+          </div>
+          
+          <div className="modal-action">
+            <button className="btn" onClick={() => SetPicker(!picker)}>+</button>
+            <label htmlFor="my-modal" className="btn">Done</label>
+          </div>
+
+        </div>
+      </div>
+      
+    </>
+  );
+};
+
+export default PickerModal;
--- app/src/pages/UserMainPage/index.jsx
+++ app/src/pages/UserMainPage/index.jsx
@@ -1,13 +1,35 @@
 import TeacherCard from "../../components/TeacherCard";
+import CalendarPanel from "../../components/CalendarPanel";
+import PickerModal from "../../components/PickerModal";
+import React from "react";
 
 const UserMainPage = () => {
+
     return (
-        <div>
+        <>
             <div>
-                <h1 className="text-3xl font-bold text-center my-8">Teacher's Offers</h1> {/* Title */}
-                <TeacherCard />
+                {/*<h1 className="text-3xl font-bold text-center my-8">Teacher's Offers</h1> {/* Title */}
+                <div className="">
+                    <div className="flex flex-row">
+                    <div className="basis-[20%] mt-5"><CalendarPanel /></div>
+
+                    <div className="basis-[80%]">
+                        <div className="pt-4 ml-10 mb-10 tabs">
+                            <a className="tab tab-lg tab-lifted tab-active">
+                                <h1 className="text-3xl font-bold text-center">Search a new teacher</h1>
+                            </a>
+                            <a className="tab tab-lg tab-lifted">
+                                <h1 className="text-3xl font-bold text-center">My teachers</h1>
+                            </a>
+                        </div>
+                            
+                        <div className=""><TeacherCard /></div>
+                            <div><PickerModal /></div>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>
+        </>
     );
 }
 
